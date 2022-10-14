49f2d290ab1282f0eec2b53f4b572e38631417a4
Regina Shakirova <reginafar22@rambler.ru>
2022-10-14 10:22:12 +0400

    Решила задачи 54 и 56

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ void Zadacha54()
     Console.WriteLine("Массив, отсортированный по убыванию:");
     SortToLower(numbers);
     PrintArray(numbers);
+    Console.WriteLine();
 }
 void SortToLower(int[,] numbers)
 {
@@ -29,6 +30,7 @@ void SortToLower(int[,] numbers)
         }
     }
 }
+
 void FillArray(int[,] numbers)
 {
     Random random = new Random();
@@ -42,6 +44,7 @@ void FillArray(int[,] numbers)
         }
     }
 }
+
 void PrintArray(int[,] numbers)
 {
     int rows = numbers.GetLength(0);
@@ -55,4 +58,42 @@ void PrintArray(int[,] numbers)
         Console.WriteLine();
     }
 }
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void Zadacha56()
+{
+    Random random = new Random();
+    int rows = random.Next(3, 5);
+    int columns = random.Next(6, 8);
+    int[,] numbers = new int[rows, columns];
+    FillArray(numbers);
+    PrintArray(numbers);
+    LeastSumRow(numbers);
+}
+void LeastSumRow(int[,] numbers)
+{
+    int index = 0;
+    int minSumRow = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        int sumRow = 0;
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            sumRow = sumRow + numbers[i, j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки = {sumRow}");
+        if (i == 0)
+        {
+            minSumRow = sumRow;
+        }
+        else if (sumRow < minSumRow)
+        {
+            minSumRow = sumRow;
+            index = i;
+        }
+    }
+    Console.WriteLine($"Номер строки с минимальной суммой элементов = {index + 1} ");
+}
 Zadacha54();
+Zadacha56();
+
